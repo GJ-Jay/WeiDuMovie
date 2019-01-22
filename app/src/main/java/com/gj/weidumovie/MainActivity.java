aa276efc53ff2bd0da5c1a396c55e821f99d11a8
GJ-Jay <2481976676@qq.com>
2019-01-22 17:14:40 +0800

    测试

--- app/src/main/java/com/gj/weidumovie/MainActivity.java
+++ app/src/main/java/com/gj/weidumovie/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"请陈国星修改",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"修改",Toast.LENGTH_SHORT).show();
     }
 }
