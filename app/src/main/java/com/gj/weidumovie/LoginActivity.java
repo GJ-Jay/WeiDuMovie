0cdbcfcc8c625d3cb5245bd723a90c72d1b75c5d
sxone <44828888+sxone@users.noreply.github.com>
2019-01-22 21:45:31 +0800

    测试

---
+++ app/src/main/java/com/gj/weidumovie/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.gj.weidumovie;
+
+import com.gj.weidumovie.core.WDActivity;
+
+public class LoginActivity extends WDActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
