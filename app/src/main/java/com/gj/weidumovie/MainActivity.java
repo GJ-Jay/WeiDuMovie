04ef9bcd0b74bbff38b360b95dde16fdf0a629ea
GJ-Jay <2481976676@qq.com>
2019-01-22 21:40:16 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/gj/weidumovie/MainActivity.java

--- app/src/main/java/com/gj/weidumovie/MainActivity.java
+++ app/src/main/java/com/gj/weidumovie/MainActivity.java
@@ -1,5 +1,8 @@
 package com.gj.weidumovie;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -8,6 +11,22 @@ import com.gj.weidumovie.core.WDActivity;
 
 public class MainActivity extends WDActivity {
 
+    int count = 3;
+    /*倒计时*/
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int arg1 = msg.arg1;
+            if(count>0){
+                count--;
+                handler.sendEmptyMessageDelayed(0,1000);
+            }else{
+                startActivity(new Intent(MainActivity.this,GuidePageActivity.class));
+                finish();
+            }
+        }
+    };
 
     @Override
     protected int getLayoutId() {
@@ -16,7 +35,7 @@ public class MainActivity extends WDActivity {
 
     @Override
     protected void initView() {
-
+        handler.sendEmptyMessageDelayed(0,1000);
     }
 
     @Override
