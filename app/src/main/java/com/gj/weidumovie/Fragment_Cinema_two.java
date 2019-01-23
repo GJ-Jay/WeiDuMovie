1b85ecbe2a3136f8225a4b0285fef2cbbc946673
GJ-Jay <2481976676@qq.com>
2019-01-23 18:38:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/gj/weidumovie/MainActivity.java

---
+++ app/src/main/java/com/gj/weidumovie/Fragment_Cinema_two.java
@@ -0,0 +1,25 @@
+package com.gj.weidumovie;
+
+import com.gj.weidumovie.core.WDFragment;
+
+/**
+ * Description:<br>
+ * Author:GJ<br>
+ * Date:2019/1/23 15:10
+ */
+public class Fragment_Cinema_two extends WDFragment {
+    @Override
+    public String getPageName() {
+        return null;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_cinema_two;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
